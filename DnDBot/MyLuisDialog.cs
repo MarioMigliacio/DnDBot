fb91944cf8df2854b68bc24fe55e63673ddc427b
MarioMigliacio <migliacio@hotmail.com>
2017-05-15 19:58:40 -0700

    Races and Alignments are now supported and debugged. LUIS is getting wicked smaht

--- DnDBot/MyLuisDialog.cs
+++ DnDBot/MyLuisDialog.cs
@@ -36,6 +36,8 @@ namespace DnDBot
         private string _name = string.Empty;
         private string _gender = string.Empty;
         List<string> genderOptions = new List<string>(new string[] { "male", "female", "other", "unknown" });
+        List<string> raceOptions = new List<string>();
+        List<string> alignOptions = new List<string>();
 
         #region Player Default Intent
 
@@ -108,7 +110,7 @@ namespace DnDBot
             }
             else
             {
-                await context.PostAsync("Sorry, I did not recognize a Name value. Maybe I can learn it for next time! Usage: name [value]");
+                await context.PostAsync("Sorry, I did not recognize the Name value. Maybe I can learn it for next time! Usage: name [value]");
 
                 context.Wait(MessageReceived);
             }           
@@ -141,11 +143,11 @@ namespace DnDBot
 
         /// <summary>
         /// This PlayerGender method is called when LUIS recognizes the intent 'PlayerGender' being called.
-        /// Intended usage: gender 'desired gender' accepting options: male, female, other, and unknown.
-        /// This method also prompts a yes/no dialog to the user to lock in the name.
+        /// Intended usage: gender set. Accepting options: male, female, other, and unknown.
+        /// This method also prompts a list of choices to lock in the selected gender.
         /// </summary>
         /// <param name="context">The <see cref="IDialogContext>"/> which is passed in.</param>
-        /// <param name="result">The <see cref="IAwaitable{bool}>"/> which is passed in.</param>
+        /// <param name="result">The <see cref="LuisResult>"/> which is passed in.</param>
         /// <returns>Method awaits the completion of the Posting process.</returns>
         [LuisIntent("PlayerGender")]
         public async Task PlayerGender(IDialogContext context, LuisResult result)
@@ -163,10 +165,10 @@ namespace DnDBot
         }
 
         /// <summary>
-        /// The actual setting of our Player objects gender is done through this private asynchronous helper method.
+        /// The actual setting of our Player objects' gender is done through this private asynchronous helper method.
         /// </summary>
         /// <param name="context">The <see cref="IDialogContext>"/> which is passed in.</param>
-        /// <param name="result">The <see cref="IAwaitable{bool}>"/> which is passed in.</param>
+        /// <param name="result">The <see cref="IAwaitable{string}>"/> which is passed in.</param>
         /// <returns>Method awaits the completion of the Posting process.</returns>
         private async Task PlayerGender_Dialog(IDialogContext context, IAwaitable<string> result)
         {
@@ -187,6 +189,150 @@ namespace DnDBot
 
         #endregion
 
+        #region Player Race Intent
+
+        /// <summary>
+        /// This PlayerRace method is called when LUIS recognizes the intent 'PlayerRace' being called.
+        /// Intended usage: race set. Accepting any of the <see cref="RaceType"/> enumeration fields except 'None'.
+        /// This method also prompts a list of choices to lock in the selected race.
+        /// </summary>
+        /// <param name="context">The <see cref="IDialogContext>"/> which is passed in.</param>
+        /// <param name="result">The <see cref="LuisResult>"/> which is passed in.</param>
+        /// <returns>Method awaits the completion of the Posting process.</returns>
+        [LuisIntent("PlayerRace")]
+        public async Task PlayerRace(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("I recognized an intent to assign hero race.");
+
+            foreach (RaceType race in Enum.GetValues(typeof(RaceType)))
+            {
+                if (race != RaceType.None)
+                {
+                    raceOptions.Add(race.ToString());
+                }
+            }
+            
+            PromptOptions<string> options = new PromptOptions<string>(
+                "What race would you like your hero to be.",
+                "That was not a valid option.",
+                "You are being silly!",
+                raceOptions,
+                2);
+
+            PromptDialog.Choice(context, PlayerRace_Dialog, options);
+        }
+
+        /// <summary>
+        /// The actual setting of our Player objects' race is done through this private asynchronous helper method.
+        /// </summary>
+        /// <param name="context">The <see cref="IDialogContext>"/> which is passed in.</param>
+        /// <param name="result">The <see cref="IAwaitable{string}>"/> which is passed in.</param>
+        /// <returns>Method awaits the completion of the Posting process.</returns>
+        private async Task PlayerRace_Dialog(IDialogContext context, IAwaitable<string> result)
+        {
+            string input = await result;
+            RaceType race = RaceType.None;
+
+            switch (input)
+            {
+                case "Dwarf": race = RaceType.Dwarf; break;
+                case "Elf": race = RaceType.Elf; break;
+                case "Gnome": race = RaceType.Gnome; break;
+                case "HalfElf": race = RaceType.HalfElf; break;
+                case "HalfOrc": race = RaceType.HalfOrc; break;
+                case "Halfling": race = RaceType.Halfling; break;
+                case "Human": race = RaceType.Human; break;
+                default: race = RaceType.None; break;
+            }
+
+            if (race != RaceType.None)
+            {
+                Player.DesiredRace = race;
+                await context.PostAsync($"So your desired race: '{Player.DesiredRace}' has been set successfully.");
+            }
+            else
+            {
+                await context.PostAsync($"The race option: {race} was incorrect, please use one of the options.");
+            }
+
+            context.Wait(MessageReceived);
+        }
+
+        #endregion
+
+        #region Player Align Intent
+
+        /// <summary>
+        /// This PlayerAlign method is called when LUIS recognizes the intent 'PlayerAlign' being called.
+        /// Intended usage: align set. Accepting any of the <see cref="Alignment"/> enumeration fields except 'None'.
+        /// This method also prompts a list of choices to lock in the selected alignment.
+        /// </summary>
+        /// <param name="context">The <see cref="IDialogContext>"/> which is passed in.</param>
+        /// <param name="result">The <see cref="LuisResult>"/> which is passed in.</param>
+        /// <returns>Method awaits the completion of the Posting process.</returns>
+        [LuisIntent("PlayerAlign")]
+        public async Task PlayerAlign(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("I recognized an intent to assign hero alignment.");
+
+            foreach (Alignment align in Enum.GetValues(typeof(Alignment)))
+            {
+                if (align != Alignment.None)
+                {
+                    alignOptions.Add(align.ToString());
+                }
+            }
+
+            PromptOptions<string> options = new PromptOptions<string>(
+                "What alignment would you like your hero to be associated with.",
+                "That was not a valid option.",
+                "You are being silly!",
+                alignOptions,
+                2);
+
+            PromptDialog.Choice(context, PlayerAlign_Dialog, options);
+        }
+
+        /// <summary>
+        /// The actual setting of our Player objects' alignment is done through this private asynchronous helper method.
+        /// </summary>
+        /// <param name="context">The <see cref="IDialogContext>"/> which is passed in.</param>
+        /// <param name="result">The <see cref="IAwaitable{string}>"/> which is passed in.</param>
+        /// <returns>Method awaits the completion of the Posting process.</returns>
+        private async Task PlayerAlign_Dialog(IDialogContext context, IAwaitable<string> result)
+        {
+            string input = await result;
+            Alignment align = Alignment.None;
+
+            switch (input)
+            {
+                case "ChaoticEvil": align = Alignment.ChaoticEvil; break;
+                case "ChaoticGood": align = Alignment.ChaoticGood; break;
+                case "ChaoticNeutral": align = Alignment.ChaoticNeutral; break;
+                case "LawfulEvil": align = Alignment.LawfulEvil; break;
+                case "LawfulGood": align = Alignment.LawfulGood; break;
+                case "LawfulNeutral": align = Alignment.LawfulNeutral; break;
+                case "Evil": align = Alignment.Evil; break;
+                case "Good": align = Alignment.Good; break;
+                case "Neutral": align = Alignment.Neutral; break;
+                default: align = Alignment.None; break;
+            }
+
+            if (align != Alignment.None)
+            {
+                Player.DesiredAlign = align;
+                await context.PostAsync($"So your desired alignment: '{Player.DesiredAlign}' has been set successfully.");
+            }
+            else
+            {
+                await context.PostAsync($"The align option: {align} was incorrect, please use one of the options.");
+            }
+
+            context.Wait(MessageReceived);
+        }
+
+        #endregion
+
         #region Player Help Intent
 
         /// <summary>
@@ -198,7 +344,7 @@ namespace DnDBot
         [LuisIntent("Help")]
         public async Task HelpProcess(IDialogContext context, LuisResult result)
         {            
-            await context.PostAsync("Lets start from these basic hero fundamentals:");
+            await context.PostAsync("Lets start from these basic hero fundamentals: (you can use 'show' anytime to display currently set fields)");
 
             if (Player.Name == null)
             {
@@ -215,15 +361,15 @@ namespace DnDBot
                 await context.PostAsync("An alignment must be set. Use the command 'align set' to walk through this process.");
             }
 
-            if (Player.DesiredClass == ClassType.None)
-            {
-                await context.PostAsync("A Player Class must be set. Use the command 'class set' to walk through this process.");
-            }
-
             if (Player.DesiredRace == RaceType.None)
             {
                 await context.PostAsync("A Player Race must be set. Use the command 'race set' to walk through this process.");
             }
+
+            if (Player.DesiredClass == ClassType.None)
+            {
+                await context.PostAsync("A Player Class must be set. Use the command 'class set' to walk through this process.");
+            }            
 
             if (Player.StatsContainer.Count < 6)
             {
@@ -256,6 +402,12 @@ namespace DnDBot
             await HelpProcess(context, result);
             context.Wait(MessageReceived);
         }
+
+        #endregion
+
+        #region Info Intent
+
+
 
         #endregion
     }
